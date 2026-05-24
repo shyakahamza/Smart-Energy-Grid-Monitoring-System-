9cc2962fe92efe3d83e67d8a570dc206967124b0
shyakahamza <125553092+shyakahamza@users.noreply.github.com>
2026-05-24 05:32:35 +0200

    Update 01_schema_and_hypertables.sql

--- src/database/01_schema_and_hypertables.sql
+++ src/database/01_schema_and_hypertables.sql
@@ -1,1 +1,22 @@
+-- 1 Create energy_readings table
+CREATE TABLE energy_readings (
+    meter_id VARCHAR(10) / TEXT(10),
+    timestamp TIMESTAMPTZ NOT NULL,
+    power DOUBLE PRECISION,
+    voltage DOUBLE PRECISION,
+    current DOUBLE PRECISION,
+    frequency DOUBLE PRECISION,
+    energy DOUBLE PRECISION
+);
 
+-- Convert it to a hypertable 
+SELECT create_hypertable('energy_readings', 'timestamp', chunk_time_interval => INTERVAL '1 day');
+
+ -- 2 Create energy_readings_3h & energy_readings_week tables from energy_readings table
+CREATE TABLE energy_readings_3h (LIKE energy_readings INCLUDING ALL);
+CREATE TABLE energy_readings_week (LIKE energy_readings INCLUDING ALL);
+
+-- 3 Convert them to the respective hypertables
+
+SELECT create_hypertable('energy_readings_3h', 'timestamp', chunk_time_interval => INTERVAL '3 hours');
+SELECT create_hypertable('energy_readings_week', 'timestamp', chunk_time_interval => INTERVAL '1 week');
